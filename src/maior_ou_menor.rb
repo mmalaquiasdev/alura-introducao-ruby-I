2a848a90dc1f02f109a8515b2a80ebb4491e6326
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-19 15:19:27 -0300

    Mudando o enconding

--- src/maior_ou_menor.rb
+++ src/maior_ou_menor.rb
@@ -2,24 +2,24 @@ QNT_MAXIMA_TENTATIVAS = 5
 MAX_PONTOS_JOGADOR = 1000
 
 def dar_boas_vindas
-  puts 'Bem vindo ao jogo de adivinhacao'
+  puts 'Bem vindo ao jogo de adivinhação'
   puts 'Qual o seu nome caro jogador?'
   nome = gets
-  puts 'Ja vamos come√ßar um novo jogo para voce, ' + nome
+  puts 'Ja vamos começar um novo jogo para você, ' + nome
 end
 
 def sortear_numero_secreto
-  puts 'Tente advinhar nosso numero secreto entre 0 e 200...'
+  puts 'Tente advinhar nosso número secreto entre 0 e 200...'
   rand(200)
 end
 
 def obter_numero_escolhido_jogador
-  puts 'Informe um numero: '
+  puts 'Informe um número: '
   numero_escolhido = gets
   numero_escolhido = numero_escolhido.chomp
-  puts 'Opa! Entao voce finalmente se decidiu... '
-  puts 'Estou torcendo por voce (ou nao rsrs)'
-  puts "Sera que o nunermo #{numero_escolhido}, esta certo?"
+  puts 'Opa! Então você finalmente se decidiu... '
+  puts 'Estou torcendo por você (ou não rsrs)'
+  puts "Será que o número #{numero_escolhido}, esta certo?"
   numero_escolhido.to_i
 end
 
@@ -33,14 +33,14 @@ end
 
 def verificar_se_jogador_acertou(numero_secreto, numero_do_jogador)
   if numero_secreto_igual_numero_jogador(numero_secreto, numero_do_jogador)
-    puts 'Parabens! Parece que temos uma pessoa de sorte aqui, voce venceu :)'
+    puts 'Parabéns! Parece que temos uma pessoa de sorte aqui, você venceu :)'
     return true
   end
 
   if numero_secreto_maior_numero_jogador(numero_secreto, numero_do_jogador)
-    puts 'O numero secreto é maior!'
+    puts 'O número secreto é maior!'
   else
-    puts 'O numero secreto é menor!'
+    puts 'O número secreto é menor!'
   end
 
   false
@@ -48,7 +48,7 @@ end
 
 def informar_numeros_chutados(chute, chutes)
   chutes << chute
-  puts "Chutes ate agora: #{chutes}"
+  puts "Chutes até agora: #{chutes}"
   chutes
 end
 
@@ -61,7 +61,7 @@ def executar_jogo(qtd_max_tentativas, numero_secreto, max_pontos_jogador)
   chutes = []
   pontos_ate_agora = 0
   Array.new(qtd_max_tentativas).each_index do |tentativa|
-    puts "Voce possuiu #{qtd_max_tentativas} tentativas, essa é a de numero: #{tentativa + 1}"
+    puts "Voce possuiu #{qtd_max_tentativas} tentativas, essa é a de número: #{tentativa + 1}"
     chute = obter_numero_escolhido_jogador
     informar_numeros_chutados(chute, chutes)
     pontos_ate_agora = calcular_pontos_conquistados(chute, numero_secreto, max_pontos_jogador)
