b7e6a1729c27ab89c20c03013cd615a01aa0db15
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-19 01:51:14 -0300

    Refatorando códigos criados em funções pequenas

--- src/maior_ou_menor.rb
+++ src/maior_ou_menor.rb
@@ -1,35 +1,53 @@
-numero_secreto = 175
-numero_tentativas_possiveis = 5
+def obter_qtd_maxima_tentativas
+  numero_tentativas_possiveis = 5
+  numero_tentativas_possiveis
+end
 
 def dar_boas_vindas
   puts 'Bem vindo ao jogo de adivinhação'
   puts 'Qual o seu nome caro jogador?'
-
   nome = gets
-
   puts 'Já vamos começar um novo jogo para você, ' + nome
+end
+
+def sortear_numero_secreto
   puts 'Tente advinhar nosso número secreto entre 0 e 200...'
+  numero_sorteado = 175
+  numero_sorteado
+end
+
+def obter_numero_escolhido_jogador
+  puts 'Informe um número: '
+  numero_escolhido = gets
+  numero_escolhido = numero_escolhido.chomp
+  puts 'Opa! Então você finalmente se decidiu... '
+  puts 'Estou torcendo por você (ou não rsrs)'
+  puts "Será que o número #{numero_escolhido}, é o certo?"
+  numero_escolhido.to_i
+end
+
+def verificar_se_jogador_acertou(numero_secreto, numero_escolhido_jogador)
+  if numero_secreto == numero_escolhido_jogador
+    puts 'Paranbés! Parece que temos uma pessoa de sorte aqui, você venceu :)'
+    true
+  elsif numero_secreto > numero_escolhido_jogador
+    puts 'O número secreto é maior!'
+    false
+   else
+    puts 'O número secreto é menor!'
+    false
+  end
+end
+
+def executar_jogo(qtd_max_tentativas, numero_secreto)
+  for tentativa in 1..qtd_max_tentativas
+    puts "Você possuiu #{qtd_max_tentativas} tentativas, essa é a de número: #{tentativa}"
+    chute = obter_numero_escolhido_jogador
+    if verificar_se_jogador_acertou(numero_secreto, chute)
+      break
+    end
+  end
 end
 
 dar_boas_vindas
-
-for tentativa in 1..numero_tentativas_possiveis
-  puts "Você possuiu #{numero_tentativas_possiveis} tentativas, essa é a de número: #{tentativa}"
-  puts 'Informe um número: '
-  
-  chute = gets
-  chute = chute.chomp 
-  
-  puts 'Opa! Então você finalmente se decidiu... '
-  puts 'Estou torcendo por você (ou não rsrs)'
-  puts "Será que o número #{chute}, é o certo?"
-
-  if numero_secreto == chute.to_i
-    puts 'Paranbés! Parece que temos uma pessoa de sorte aqui, você venceu :)'
-    break
-  elsif numero_secreto > chute.to_i
-        puts 'O número secreto é maior!'
-    else
-        puts 'O número secreto é menor!'
-    end
-end
+executar_jogo(obter_qtd_maxima_tentativas, sortear_numero_secreto)
